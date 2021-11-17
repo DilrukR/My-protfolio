975d804561f417f97765ef880d6a26fba97da02a
DilrukR <68820399+DilrukR@users.noreply.github.com>
2021-11-18 00:07:19 +0530

    second commit

--- script.js
+++ script.js
@@ -16,3 +16,29 @@ function closeNav() {
   ham.style.display = "block";
   hclose.style.display = "none";
 }
+
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".hero");
+
+const secionOneOptions = {
+  rootMargin: "-100px 0px 0px 0px",
+};
+
+const sectionObserver = new IntersectionObserver(function (
+  entries,
+  sectionObserver
+) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      header.classList.add("header-active");
+    } else {
+      header.classList.remove("header-active");
+
+      console.log(entry.target);
+    }
+  });
+},
+secionOneOptions);
+
+sectionObserver.observe(header);
+sectionObserver.observe(sectionOne);
